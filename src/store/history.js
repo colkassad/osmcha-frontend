108f65a98ac8455b33fc074807613213d8b77dd1
patrick.doody <patrick.doody@rgi-corp.com>
2022-04-21 13:34:47 -0400

    Remove Source

--- src/store/history.js
+++
@@ -1,9 +0,0 @@
-import { createBrowserHistory } from 'history';
-import { isDev } from '../config';
-let historyConfig = {};
-if (isDev) {
-  historyConfig.basename = '/osmcha-frontend';
-}
-
-const history = createBrowserHistory(historyConfig);
-export { history };
