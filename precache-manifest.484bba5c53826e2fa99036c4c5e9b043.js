684e25c06c475410b903eaa3e86e180f68e831a3
patrick.doody <patrick.doody@rgi-corp.com>
2022-04-21 13:33:43 -0400

    Add Static

---
+++ precache-manifest.484bba5c53826e2fa99036c4c5e9b043.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b386a8a9e32eee96f9a3c627258f7409",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3306c47cc97dfef989d",
+    "url": "/static/css/2.e6a3113b.chunk.css"
+  },
+  {
+    "revision": "5c345c20a01674735ac3",
+    "url": "/static/css/main.2bb5fb54.chunk.css"
+  },
+  {
+    "revision": "d3306c47cc97dfef989d",
+    "url": "/static/js/2.df8bbe99.chunk.js"
+  },
+  {
+    "revision": "a6c9602f8c24a2c52bf9a0e87b391e18",
+    "url": "/static/js/2.df8bbe99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebdc5a489e89f92c6f2a",
+    "url": "/static/js/3.40cecf6e.chunk.js"
+  },
+  {
+    "revision": "cc111eb067dd9513fd4e32ee464287ae",
+    "url": "/static/js/3.40cecf6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c345c20a01674735ac3",
+    "url": "/static/js/main.27842720.chunk.js"
+  },
+  {
+    "revision": "962cb5d4bac3a87df71d",
+    "url": "/static/js/runtime-main.55a7fff9.js"
+  },
+  {
+    "revision": "ed2bb0d2fecd447f6a74dd7581448589",
+    "url": "/static/media/banner.ed2bb0d2.png"
+  },
+  {
+    "revision": "7e04a82a7e23f2adb03169d23beaa602",
+    "url": "/static/media/thumbs-down.7e04a82a.svg"
+  },
+  {
+    "revision": "13d765c5811fc9db3eba173459a6a762",
+    "url": "/static/media/thumbs-up.13d765c5.svg"
+  },
+  {
+    "revision": "195ff4f9fd1d7c4ac421ac9623b9c61f",
+    "url": "/static/media/work_flow.195ff4f9.png"
+  }
+]);
